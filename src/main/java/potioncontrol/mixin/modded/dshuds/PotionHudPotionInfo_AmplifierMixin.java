70d58a6169765008db50f6f2b829c23a15e632be
nischhelm <nischhelm@gmail.com>
2026-03-09 23:07:35 +0100

    finish descriptions

--- src/main/java/potioncontrol/mixin/modded/dshuds/PotionHudPotionInfo_AmplifierMixin.java
+++ src/main/java/potioncontrol/mixin/modded/dshuds/PotionHudPotionInfo_AmplifierMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import potioncontrol.util.PotionInfo;
 
 @Mixin(targets = "org.orecruncher.dshuds.hud.PotionHUD$PotionInfo")
 public abstract class PotionHudPotionInfo_AmplifierMixin {
@@ -21,12 +22,15 @@ public abstract class PotionHudPotionInfo_AmplifierMixin {
     @Inject(method = "<init>", at = @At(value = "TAIL"), remap = false)
     private void pc_displayCorrectAmplifier(PotionEffect effect, CallbackInfo ci) {
         if(this.potion.shouldRenderInvText(this.potionEffect)) return;
-
-        String newText = Localization.format(this.potion.getName());
-        if(this.potionEffect.getAmplifier() > 0 && this.potionEffect.getAmplifier() < 10)
-            newText += " " + Localization.format("enchantment.level." + (this.potionEffect.getAmplifier() + 1));
-        else if(this.potionEffect.getAmplifier() < 0 || this.potionEffect.getAmplifier() >= 10)
-            newText += " " + (this.potionEffect.getAmplifier() + 1);
-        this.effectText = newText;
+        PotionInfo info = PotionInfo.get(this.potion);
+        if(info != null) this.effectText = info.getTranslatedName(this.potion, this.potionEffect.getAmplifier());
+        else {
+            String newText = Localization.format(this.potion.getName());
+            if (this.potionEffect.getAmplifier() > 0 && this.potionEffect.getAmplifier() < 10)
+                newText += " " + Localization.format("enchantment.level." + (this.potionEffect.getAmplifier() + 1));
+            else if (this.potionEffect.getAmplifier() < 0 || this.potionEffect.getAmplifier() >= 10)
+                newText += " " + (this.potionEffect.getAmplifier() + 1);
+            this.effectText = newText;
+        }
     }
 }
--- src/main/java/potioncontrol/mixin/vanilla/InventoryEffectRendererMixin.java
+++ src/main/java/potioncontrol/mixin/vanilla/InventoryEffectRendererMixin.java
@@ -5,13 +5,10 @@ import net.minecraft.client.renderer.InventoryEffectRenderer;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.text.TextFormatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import potioncontrol.util.PotionInfo;
-
-import java.util.stream.Collectors;
 
 @Mixin(InventoryEffectRenderer.class)
 public abstract class InventoryEffectRendererMixin {
@@ -21,14 +18,16 @@ public abstract class InventoryEffectRendererMixin {
     )
     private String pc_modifyName(String text, @Local Potion potion, @Local PotionEffect effect) {
         PotionInfo info = PotionInfo.get(potion);
-        if(info != null) text = info.getTranslatedName(potion, effect.getAmplifier());
-        else text = I18n.format(potion.getName());
+        if(info != null) return info.getTranslatedName(potion, effect.getAmplifier());
+        else {
+            text = I18n.format(potion.getName());
 
-        int amp = effect.getAmplifier();
-        if(amp > 0 && amp < 10)
-            text += " " + I18n.format("enchantment.level."+(amp+1));
-        else if(amp != 0) text = text + " " + (amp + 1); //lvl 11+ or negative lvls
+            int amp = effect.getAmplifier();
+            if (amp > 0 && amp < 10)
+                text += " " + I18n.format("enchantment.level." + (amp + 1));
+            else if (amp != 0) text = text + " " + (amp + 1); //lvl 11+ or negative lvls
 
-        return text;
+            return text;
+        }
     }
 }
