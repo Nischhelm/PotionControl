b6a98b83f8aeca85f49ec2bf017de08518902b10
nischhelm <nischhelm@gmail.com>
2026-04-18 19:43:12 +0200

    v1.0.1
    - fixed strength and weakness attribute modfiiers not being modifiable via json
    
    added the following options to the potion jsons
    
    - blacklistedEntities (list of modid:mobid)
    - blacklistedTags (list of any tags to search in mobs "Tags" nbt)
    - blocksApplicationOf (list of modid:potionid potions that cant be applied when this potion is active)
    - removesOnApplication (list of modid:potionid potions that will be removed from the entity when this potion is added)

--- src/main/java/potioncontrol/PotionControl.java
+++ src/main/java/potioncontrol/PotionControl.java
@@ -45,7 +45,7 @@ import java.util.Map;
 )
 public class PotionControl {
     public static final String MODID = "potioncontrol";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "PotionControl";
     public static final Logger LOGGER = LogManager.getLogger(PotionControl.NAME);
     public static Configuration CONFIG = null;
