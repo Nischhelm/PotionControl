236f1478f1aae13f3f21bd70cdaf2ef39c8449eb
nischhelm <nischhelm@gmail.com>
2026-09-17 16:28:53 +0200

    fix cfg comments

--- src/main/java/potioncontrol/config/folders/BlacklistConfig.java
+++ src/main/java/potioncontrol/config/folders/BlacklistConfig.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class BlacklistConfig {
     @Config.Comment({
             "Potions in this list will be prevented from being registered in the game. There will be no way to access them at all.",
-            "If \"Potions disable their types\" is enabled, this will also de-register all connected potiontypes that only have this potion as an effect."
+            "If \"Potions disable their types\" is enabled, this will also unregister all connected potiontypes that only have this potion as an effect."
     })
     @Config.Name("Registered Potion Blacklist")
     @Config.RequiresMcRestart
@@ -22,8 +22,8 @@ public class BlacklistConfig {
     @Config.Comment({
             "Most potion types are directly tied to a specific potion, as they only give that potion as an effect",
             "Example: long_fire_resistance gives the effect of type fire_resistance, and no other effects",
-            "This toggle makes it so de-registering potions will automatically also de-register all potion types that only map to this potion",
-            "Disabling this toggle will force you to manually blacklist all connected types for each potion you de-register."
+            "This toggle makes it so unregistering potions will automatically also unregister all potion types that only map to this potion",
+            "Disabling this toggle will force you to manually blacklist all connected types for each potion you unregister."
     })
     @Config.Name("Potions disable their types")
     public boolean potionsDisableTheirTypes = true;
